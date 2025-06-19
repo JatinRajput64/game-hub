1f3cda08526f282bd9e47b8b1a4fbe10e336583b
Jatin Kumar <jatinrajput64@gmail.com>
2025-06-19 22:36:24 +0530

    navigating to home page from navigation bar

--- src/components-files/SearchInput.tsx
+++ src/components-files/SearchInput.tsx
@@ -2,10 +2,12 @@ import useGameQueryStore from "@/Store";
 import { Input, InputGroup } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   return (
     <form
@@ -13,6 +15,7 @@ const SearchInput = () => {
         event.preventDefault();
 
         if (ref.current) setSearchText(ref.current.value);
+        navigate("/");
       }}
     >
       <InputGroup startElement={<BsSearch />}>
